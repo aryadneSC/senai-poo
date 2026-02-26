e920c60dcc764d9ff78ab5893051727818fb9cbd
nemine <aryadne.campos@ba.estudante.senai.br>
2026-02-26 10:13:39 -0300

    criado class Macaco

--- JAVA/SENAI/Aula03_Exercicios_pt2/Macaco.java
+++ JAVA/SENAI/Aula03_Exercicios_pt2/Macaco.java
@@ -1,9 +1,25 @@
-public class Macaco {
+import java.util.ArrayList;
+import java.util.List;
+
+class Macaco {
+    private String nome;
+    private List<String> estomago;
     
+    public Macaco(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNome() {
+        return this.nome;
+    }
 }
 
 public class Main {
 	public static void main(String[] args) {
 		
+		Macaco meuMacaco = new Macaco("Chico");
+		System.out.println("Macaco " + meuMacaco.getNome() + " nasceu!");
+		
+		//TODO add comer(String alimento), verEstomago()
 	}
 }
