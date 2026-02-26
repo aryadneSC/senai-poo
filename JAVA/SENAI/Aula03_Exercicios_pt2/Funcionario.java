347e77c6c0a0441ba1c128aca297b634f070eb5d
nemine <aryadne.campos@ba.estudante.senai.br>
2026-02-26 09:51:14 -0300

    Atualizar o Funcionario.java

--- JAVA/SENAI/Aula03_Exercicios_pt2/Funcionario.java
+++ JAVA/SENAI/Aula03_Exercicios_pt2/Funcionario.java
@@ -4,6 +4,8 @@ class Funcionario {
 
     public Funcionario(String nome) {
         this.nome = nome;
+        // TODO set salary to 0.0 (DONE)
+        this.salario = 0.0; 
     }
 
     public String getNome() {
@@ -19,7 +21,8 @@ class Funcionario {
     }
 
     public void aumentarSalario(double percentualAumentoSalario) {
-        double valorAumento = this.salario * (percentualAumentoSalario / 100);
+        double valorAumento = this.salario *
+(percentualAumentoSalario / 100);
         this.salario += valorAumento;
     }
 }
@@ -27,11 +30,14 @@ class Funcionario {
 public class Exercicio01 {
     public static void main(String[] args) {
         Funcionario meuFuncionario = new Funcionario("Ana");
+        
         System.out.println("Nome do funcionario: " + meuFuncionario.getNome());
 
+        // Atribuite Ana's actual salary
         meuFuncionario.setSalario(2000);
-        System.out.println("Salario inicial: R$ " + meuFuncionario.getSalario());
+        System.out.println("Salario inicial R$ " + meuFuncionario.getSalario());
 
+        // add salary increment
         meuFuncionario.aumentarSalario(10);
         System.out.println("Salario final após 10% de aumento: R$ " + meuFuncionario.getSalario());
     }
