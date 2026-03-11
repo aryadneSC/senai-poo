c7b3988a5e2a4c7d75c9af03e4fac60c4f9f5b2b
nemine <aryadne.campos@ba.estudante.senai.br>
2026-03-11 14:15:03 -0300

    Add getter and setter methods to Person class

--- JAVA/SENAI/Aula05_Lista_Exercicios/Anexo4.5_Lista_Introducao_a_POO_com_JAVA/src/Person/Person.java
+++ JAVA/SENAI/Aula05_Lista_Exercicios/Anexo4.5_Lista_Introducao_a_POO_com_JAVA/src/Person/Person.java
@@ -14,10 +14,28 @@ public class Person {
     private int age;
     private double height;
 
-    public Person(String name, int age, double height) {
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
         this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setHeight(double height) {
         this.height = height;
+    }
+
+    public double getHeight() {
+        return this.height;
     }
 
     public void displayData() {
