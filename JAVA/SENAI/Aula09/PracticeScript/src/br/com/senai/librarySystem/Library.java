3d7d290ca3ebfd065cdad67e8da74cb54a82ac96
aryadneSC <seu-email@exemplo.com>
2026-04-17 19:33:43 -0300

    feat: add Library class and its methods

---
+++ JAVA/SENAI/Aula09/PracticeScript/src/br/com/senai/librarySystem/Library.java
@@ -0,0 +1,24 @@
+package br.com.senai.librarySystem;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Library {
+	private List<String> LibrarySource = new ArrayList<>();
+	
+	public void addSource(LibrarySource material) {
+		
+	}
+	
+	public void readSources() {
+		
+	}
+	
+	public void findByTitle(String title) {
+		
+	}
+	
+	public void borrowSource(LibrarySource material) {
+		
+	}
+}
